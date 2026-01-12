3026142a9b8547ce445b973259d123e03e53bd0a
Aatif Mashkoor <mraatif05@gmail.com>
2026-01-13 00:46:15 +0530

    Update Problem_2.py

--- Problem_2.py
+++ Problem_2.py
@@ -1,3 +1,5 @@
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
 a = 1
 b = 2
 total = 0
@@ -14,3 +16,4 @@ print(total)
 
 
     
+
